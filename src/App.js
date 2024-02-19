54d9c2da05ba9ad4f180d1d6f2cd6dad0d12a274
Prince Yadav <99ecnirp@gmail.com>
2024-02-19 11:03:26 +0530

    Project setup

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="text-3xl font-bold text-red-700">
+      Namaste Everyone
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
