9f0c2b7321919492db3743c70a0557be6a0c58d9
Prince Yadav <99ecnirp@gmail.com>
2024-03-01 04:07:36 +0530

    mae custom hook

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,20 +1,10 @@
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
 import Header from './Header'
-import { API_OPTIONS } from '../utils/constant';
-import { useEffect } from 'react';
+
 
 const Browse = () => {
 
-  const getNowPlayingMovies = async () => {
-    const url = 'https://api.themoviedb.org/3/movie/now_playing?page=1';
-    fetch(url, API_OPTIONS)
-      .then(res => res.json())
-      .then(json => console.log("------------------------",json))
-      .catch(err => console.error('error:' + err));
-  }
-
-  useEffect(()=>{
-    getNowPlayingMovies();
-  }, []);
+  useNowPlayingMovies();
 
   return (
     <div>
---
+++ src/hooks/useNowPlayingMovies.js
@@ -0,0 +1,25 @@
+import { API_OPTIONS } from '../utils/constant';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addNowPlayingMovies } from '../utils/moviesSlice';
+
+const useNowPlayingMovies = () => {
+    const dispatch = useDispatch();
+
+    const getNowPlayingMovies = async () => {
+        const url = 'https://api.themoviedb.org/3/movie/now_playing?page=1';
+        fetch(url, API_OPTIONS)
+        .then(res => res.json())
+        .then(json => {
+            dispatch(addNowPlayingMovies(json));
+            console.log("------------------------",json)
+        })
+        .catch(err => console.error('error:' + err));
+    }
+
+    useEffect(()=>{
+        getNowPlayingMovies();
+    }, []);
+}
+
+export default useNowPlayingMovies;
